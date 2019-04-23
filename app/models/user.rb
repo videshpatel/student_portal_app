487fb80953dccc8fe664176c96957ac98ffe573a
“Videsh <videsh.patel@gmail.com>
2019-04-22 19:25:43 -0500

    made small tweak

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
